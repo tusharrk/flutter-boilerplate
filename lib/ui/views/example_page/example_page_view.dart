aa551ed8e67902736a0c12cdcfe541c171710434
Tushar Kalsara <tushar.kalsara.ext@nmims.edu>
2025-04-19 16:55:56 +0530

    changes

--- lib/ui/views/example_page/example_page_view.dart
+++ lib/ui/views/example_page/example_page_view.dart
@@ -1,11 +1,13 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_boilerplate/core/common_imports/ui_imports.dart';
+import 'package:flutter_boilerplate/gen/asset/assets.gen.dart';
 import 'package:flutter_boilerplate/ui/components/widgets/base/app_app_bar.dart';
 import 'package:flutter_boilerplate/ui/components/widgets/base/app_button.dart';
 import 'package:flutter_boilerplate/ui/components/widgets/base/app_scaffold.dart';
 import 'package:flutter_boilerplate/ui/components/widgets/base/app_text_field.dart';
 import 'package:flutter_boilerplate/ui/components/widgets/base/stacked_tabbed_view.dart';
+import 'package:flutter_boilerplate/ui/components/widgets/custom_image/custom_network_image.dart';
 
 import 'example_page_viewmodel.dart';
 
@@ -42,85 +44,86 @@ class ExamplePageView extends StackedTabbedView<ExamplePageViewModel> {
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 25.0),
           child: Center(
-            child: Column(
-              mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                verticalSpaceLarge,
-                Column(
-                  children: [
-                    Text(
-                      'home.title'.tr(),
-                      style: const TextStyle(
-                        fontSize: 35,
-                        fontWeight: FontWeight.w900,
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  verticalSpaceLarge,
+                  Column(
+                    children: [
+                      Text(
+                        'home.title'.tr(),
+                        style: const TextStyle(
+                          fontSize: 35,
+                          fontWeight: FontWeight.w900,
+                        ),
                       ),
-                    ),
-                    //Assets.images.thumbnailLogo.image(),
-                    AppTextField(
-                      label: 'Email',
-                      hintText: 'you@example.com',
-                      controller: viewModel.emailController,
-                      validator: viewModel.validateEmail,
-                      keyboardType: TextInputType.emailAddress,
-                      isFilled: false,
-                    ),
-                    verticalSpaceMedium,
-                    AppButton(
-                      label: 'Login',
-                      onPressed: () => print('Logging in...'),
-                      isBusy: false,
-                      icon: const Icon(Icons.login),
-                      size: AppButtonSize.medium,
-                      fullWidth: true,
-                    ),
-                    OutlinedButton(
-                        onPressed: () {}, child: const Text("adasdas")),
-                    verticalSpaceMedium,
-
-                    MaterialButton(
-                      onPressed: viewModel.incrementCounter,
-                      child: Text(
-                        viewModel.counterLabel,
-                        style: const TextStyle(),
+                      Assets.images.thumbnailLogo.image(),
+                      AppTextField(
+                        label: 'Email',
+                        hintText: 'you@example.com',
+                        controller: viewModel.emailController,
+                        validator: viewModel.validateEmail,
+                        keyboardType: TextInputType.emailAddress,
+                        isFilled: false,
                       ),
-                    ),
-                  ],
-                ),
-                Switch(
-                  value: viewModel.isDarkMode,
-                  onChanged: (value) {
-                    viewModel.changeDarkMode(value);
-                  },
-                ),
-                // const CustomNetworkImage(
-                //   imageUrl: 'https://picsum.photos/id/1/140/200',
-                //   emptyWidget: Icon(Icons.error),
-                //   boxFit: BoxFit.cover,
-                //   size: Size(150, 200),
-                // ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    MaterialButton(
-                      color: kcDarkGreyColor,
-                      onPressed: viewModel.showDialog,
-                      child: const Text(
-                        'Show Dialog',
-                        style: TextStyle(),
+                      verticalSpaceMedium,
+                      AppButton(
+                        label: 'Login',
+                        onPressed: () => print('Logging in...'),
+                        isBusy: false,
+                        icon: const Icon(Icons.login),
+                        size: AppButtonSize.medium,
+                        fullWidth: true,
                       ),
-                    ),
-                    MaterialButton(
-                      color: kcDarkGreyColor,
-                      onPressed: viewModel.showBottomSheet,
-                      child: const Text(
-                        'Show Bottom Sheet',
-                        style: TextStyle(),
+                      OutlinedButton(
+                          onPressed: () {}, child: const Text("adasdas")),
+                      verticalSpaceMedium,
+                      MaterialButton(
+                        onPressed: viewModel.incrementCounter,
+                        child: Text(
+                          viewModel.counterLabel,
+                          style: const TextStyle(),
+                        ),
                       ),
-                    ),
-                  ],
-                ),
-              ],
+                    ],
+                  ),
+                  Switch(
+                    value: viewModel.isDarkMode,
+                    onChanged: (value) {
+                      viewModel.changeDarkMode(value);
+                    },
+                  ),
+                  const CustomNetworkImage(
+                    imageUrl: 'https://picsum.photos/id/1/140/200',
+                    emptyWidget: Icon(Icons.error),
+                    boxFit: BoxFit.cover,
+                    size: Size(50, 80),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      MaterialButton(
+                        color: kcDarkGreyColor,
+                        onPressed: viewModel.showDialog,
+                        child: const Text(
+                          'Show Dialog',
+                          style: TextStyle(),
+                        ),
+                      ),
+                      MaterialButton(
+                        color: kcDarkGreyColor,
+                        onPressed: viewModel.showBottomSheet,
+                        child: const Text(
+                          'Show Bottom Sheet',
+                          style: TextStyle(),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ),
